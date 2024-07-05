36411d5b164aef30aa39df10037a616d83965ed3
s1f102201391 <s1f102201391@iniad.org>
2024-07-06 05:32:11 +0900

    1年w

--- server/domain/work/repository/workQuery.ts
+++ server/domain/work/repository/workQuery.ts
@@ -41,7 +41,7 @@ const toWorkEntity = (prismaWork: Work): WorkEntity => {
         imageUrl: null,
         errorMsg: z.string().parse(prismaWork.errorMsg),
       };
-    /*v8 ignore next 2 */
+    /* v8 ignore next 2 */
     default:
       throw new Error(status satisfies never);
   }
