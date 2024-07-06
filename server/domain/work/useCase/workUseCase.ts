a9c95e049e8b4fba5f69762149a1c372a0841688
Nikesuiden <taiki.nikewp@gmail.com>
2024-07-06 10:23:04 +0900

    バグ解決

--- server/domain/work/useCase/workUseCase.ts
+++ server/domain/work/useCase/workUseCase.ts
@@ -5,7 +5,7 @@ import { workMethod } from '../model/workMethod';
 import { novelQuery } from '../repository/novelQuery';
 import { workCommand } from '../repository/workCommand';
 import { s3 } from 'service/s3Client';
-import { getContentKey } from '../service/getS3Key';
+import { getContentKey, getImageKey } from '../service/getS3Key';
 
 export const workUseCase = {
   create: (novelUrl: string): Promise<loadingWorkEntity> =>
@@ -24,14 +24,14 @@ export const workUseCase = {
     }),
   complete: (loadingWork: loadingWorkEntity, image: Buffer): Promise<void> =>
     transaction('RepeatableRead', async (tx) => {
-      const completeWork = await workMethod.complete(loadingWork);
+      const completedWork = await workMethod.complete(loadingWork);
 
-      await workCommand.save(tx, completeWork);
-      await s3.putImage(`works/${loadingWork.id}/image.png`, image)
+      await workCommand.save(tx, completedWork);
+      await s3.putImage(getImageKey(loadingWork.id), image)
     }),
-  failure: (leadingWork: loadingWorkEntity, errorMsg: string): Promise<void> =>
+  failure: (loadingWork: loadingWorkEntity, errorMsg: string): Promise<void> =>
     transaction('RepeatableRead', async (tx) => {
-      const failedWork = await workMethod.failure(leadingWork, errorMsg);
+      const failedWork = await workMethod.failure(loadingWork, errorMsg);
       await workCommand.save(tx, failedWork);
     }),
 };
